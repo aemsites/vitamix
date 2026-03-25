7296130ae0033f0ec984504f39128aa0e8258844
David Nuescheler <david@nuescheler.ch>
2026-03-25 09:37:51 -0600

    chore: one more image unmapped rewrite path

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1176,7 +1176,9 @@ async function loadEager(doc) {
   }
 
   /* adjust shop images to locale root path, util all of shop is mapped */
-  if (window.location.pathname.includes('/shop/') || window.location.pathname.includes('/commercial/')) {
+  if (window.location.pathname.includes('/shop/')
+    || window.location.pathname.includes('/commercial/')
+    || window.location.pathname.includes('/catalog/product_compare/')) {
     const images = doc.querySelectorAll('img[src^="./media_"]');
     images.forEach((img) => {
       img.setAttribute('src', img.getAttribute('src').replace('./media_', '/us/en_us/media_'));
