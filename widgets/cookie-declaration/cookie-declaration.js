938110d181279d5bac84b9774e5a1c96f32222c3
fkakatie <ksexton@adobe.com>
2026-05-21 16:38:53 -0600

    fix: inject cookiebot script via createElement instead of loadScript

--- widgets/cookie-declaration/cookie-declaration.js
+++ widgets/cookie-declaration/cookie-declaration.js
@@ -1,5 +1,12 @@
-import { loadScript } from '../../scripts/aem.js';
-
-export default async function decorate() {
-  await loadScript('https://consent.cookiebot.com/1d1d4c74-9c10-49e5-9577-f8eb4ba520fb/cd.js');
+/**
+ * Injects the Cookiebot Cookie Declaration script into the widget.
+ * Cookiebot renders the declaration in place of its script element when it loads.
+ * @param {Element} widget - The widget element to inject into
+ */
+export default function decorate(widget) {
+  const script = document.createElement('script');
+  script.id = 'CookieDeclaration';
+  script.src = 'https://consent.cookiebot.com/1d1d4c74-9c10-49e5-9577-f8eb4ba520fb/cd.js';
+  script.async = true;
+  widget.append(script);
 }
